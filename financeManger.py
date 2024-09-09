7f2796a95318a3f2d8c5b2d6f18375782cb479fb
Chanel Morgan <chanelmorgan05@icloud.com>
2024-09-09 20:29:04 +0100

    reading csv file

---
+++ financeManger.py
@@ -0,0 +1,14 @@
+#T his is an example, have to get actual file from bank app and check the format
+
+import csv
+
+# Reading the file
+MONTH = 'may'
+
+file= f"hsbc_{MONTH}.csv"
+
+with open(file, mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        print(row)
+
